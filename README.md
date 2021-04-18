f621bd454faf4ec29f29cafe195867b052349170
dgyarmati <dpolya.w@gmail.com>
2021-04-18 14:54:57 +0200

    initializing board
    initilizing board
    
    removed binary file
    
    minor refactors; added helper functions for making a move and determining an empty square; scaffolded main loop
    
    removed .cc file
    
    amended README.md
    
    added user input handling
    
    implementing game logic
    
    finalizing game
    
    fixed bug which caused player to win at first turn
    
    ai now tries to find three in a row
    
    added ai blocking move

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# A Tic Tac Toe clone written in C.
+# Tic Tac Toe
+
+A Tic Tac Toe clone written in C. Based on this tutorial series:
+https://www.youtube.com/playlist?list=PLZ1QII7yudbc7_ZgXA-gIXmME41Rs2GP5
---
+++ src/tictactoe.cpp
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctime>
+
+// TODO: direction the player can choose should be called squareNumber or similar
+// TODO: bugfix - draw is displayed even if there's a win
+// TODO: bugfix - the game goes on for one round sometimes even if there's a row of three
+// TODO: lastMove should be called gameAreIdxToMove or similar
+
+// human side uses noughts, comp side uses crosses
+enum { NOUGHTS, CROSSES, BORDER, EMPTY };
+enum { HUMANWIN, COMPWIN, DRAW };
+
+// directions to search for three in a rows
+const int directions[4] = {1, 5, 4, 6};
+
+const int gameAreaIndices[9] = {
+    6, 7, 8, 
+    11, 12, 13, 
+    16, 17, 18
+};
+
+const int middleIdxInGameArea = 4;
+const int cornerIndices[4] = {0, 2, 6, 8};
+
+int getNoOfSameSymbolsInADirection(int startSquareIdx, const int direction, const int *board, const int currentSymbol) {
+    int noOfSameSymbolsInARow = 0;
+
+    while (board[startSquareIdx] != BORDER) {
+        if (board[startSquareIdx] != currentSymbol) {
+            break;
+        }
+        noOfSameSymbolsInARow++;
+        startSquareIdx += direction;
+    }
+
+    return noOfSameSymbolsInARow;
+}
+
+int findThreeInARow(const int *board, const int currentSymbolIdx, const int currentSymbol) {
+    int direction = 0;
+    int threeCount = 0;
+
+    for (int i = 0; i < 4; i++) {
+        direction = directions[i];
+        threeCount += getNoOfSameSymbolsInADirection(currentSymbolIdx + direction, direction, board, currentSymbol);
+        threeCount += getNoOfSameSymbolsInADirection(currentSymbolIdx + direction * -1, direction * -1, board, currentSymbol);
+
+        if (threeCount == 3) {
+            break;
+        }
+
+        threeCount = 1;
+    }
+
+    return threeCount;
+}
+
+void initializeBoard(int *board) {
+    int i = 0;
+    for (i = 0; i < 25; i++) {
+        board[i] = BORDER;
+    }
+    for (i = 0; i < 9; i++) {
+        board[gameAreaIndices[i]] = EMPTY;
+    }
+}
+
+void drawBoard(const int *board) {
+    char boardSymbols[] = "OX|-"; // noughts, crosses, board limit, empty
+
+    printf("\n\nBoard:\n\n");
+
+    for (int i = 0; i < 9; i++) {
+        if (i != 0 && i % 3 == 0) {
+            printf("\n\n");
+        }
+        printf("%4c", boardSymbols[board[gameAreaIndices[i]]]);
+    }
+    printf("\n");
+}
+
+int getHumanPlayerMove(const int *board) {
+    char userInput[4];
+
+    int moveConfirmed = 0;
+    int direction = -1;
+
+    while (moveConfirmed == 0) {
+        printf("Please enter a direction from 1 to 9: ");
+        fgets(userInput, 3, stdin);
+        fflush(stdin);
+
+        if (strlen(userInput) != 2) {
+            printf("Invalid input! I need a direction followed by an Enter.\n");
+            continue;
+        }
+
+        if (sscanf(userInput, "%d", &direction) != 1) {
+            direction = -1;
+            printf("Invalid input!\n");
+            continue;
+        }
+
+        if (direction < 1 || direction > 9) {
+            direction = -1;
+            printf("Invalid direction!\n");
+            continue;
+        }
+
+        direction--; // zero indexing (the input is from 1 - 9, so we need to convert it to a valid index)
+
+        if (board[gameAreaIndices[direction]] != EMPTY) {
+            direction = -1;
+            printf("Square is already taken!\n");
+            continue;
+        }
+
+        moveConfirmed = 1;
+    }
+
+    printf("Moved in direction: %d\n", (direction + 1));
+    return gameAreaIndices[direction];
+}
+
+int findNextBestMove(const int *board) {
+    int direction = gameAreaIndices[middleIdxInGameArea];
+
+    // check if middle of the board is available
+    if (board[direction] == EMPTY) {
+        return direction;
+    }
+
+    direction = -1;
+
+    // check if the corners are available
+    for (int i = 0; i < 4; i++) {
+        direction = gameAreaIndices[cornerIndices[i]];
+        if (board[direction] == EMPTY) {
+            break;
+        }
+        direction = -1;
+    }
+
+    return direction;
+}
+
+int attemptWinningMove(int *board, const int currentPlayer) {
+    // checks if next move can win the game
+
+    int direction = 1;
+    int isWinningMoveFound = 0;
+
+    for (int i = 0; i < 9; i++) {
+        if (board[gameAreaIndices[i]] == EMPTY) {
+            direction = gameAreaIndices[i];
+            board[direction] = currentPlayer;
+
+            if (findThreeInARow(board, direction, currentPlayer) == 3) {
+                isWinningMoveFound = 1;
+            }
+
+            board[direction] = EMPTY;
+            if (isWinningMoveFound == 1) {
+                break;
+            }
+            direction = -1;
+        }
+    }
+    return direction;
+}
+
+int getAIPlayerMove(int *board, const int currentPlayer) {
+    int freeSquareIdx = 0;
+    int availableDirections[9];
+    int randDirection = 0;
+
+    randDirection = attemptWinningMove(board, currentPlayer);
+    if (randDirection != -1) {
+        return randDirection;
+    }
+
+    // find winning move for opponent, and attempt to block it
+    randDirection = attemptWinningMove(board, currentPlayer ^ 1);
+    if (randDirection != -1) {
+        return randDirection;
+    }
+
+    randDirection = findNextBestMove(board);
+    if (randDirection != -1) {
+        return randDirection;
+    }
+
+    randDirection = 0;
+    for (int i = 0; i < 9; i++) {
+        if (board[gameAreaIndices[i]] == EMPTY) {
+            availableDirections[freeSquareIdx++] = gameAreaIndices[i];
+        }
+    }
+
+    randDirection = (rand() % freeSquareIdx);
+    return availableDirections[randDirection];    
+}
+
+void makeMove(int *board, const int square, const int currentPlayer) {
+    board[square] = currentPlayer;
+}
+
+int hasEmptySquare(const int *board) {
+    for (int i = 0; i < 9; i++) {
+        if (board[gameAreaIndices[i]] == EMPTY) return 1;
+    }
+    return 0;
+}
+
+void run() {
+    int gameOver = 0;
+    int currentPlayerSymbol = NOUGHTS;
+    int lastMove = 0;
+    int board[25];
+
+    initializeBoard(&board[0]);
+    drawBoard(&board[0]);
+
+    while (!gameOver) {
+        if (currentPlayerSymbol == NOUGHTS) {
+            lastMove = getHumanPlayerMove(&board[0]);
+            makeMove(&board[0], lastMove, currentPlayerSymbol);
+            currentPlayerSymbol = CROSSES;
+        } else {
+            lastMove = getAIPlayerMove(&board[0], currentPlayerSymbol);
+            makeMove(&board[0], lastMove, currentPlayerSymbol);
+            currentPlayerSymbol = NOUGHTS;
+            drawBoard(&board[0]);
+        }
+
+        int oppositePlayerSymbol = currentPlayerSymbol ^ 1;
+        if (findThreeInARow(board, lastMove, oppositePlayerSymbol) == 3) {
+            printf("Game over!\n");
+            gameOver = 1;
+            if (currentPlayerSymbol == NOUGHTS) {
+                printf("Computer wins!\n");
+            } else {
+                printf("Player wins!\n");
+            }
+        }
+
+        if (!hasEmptySquare(board)) {
+            printf("Game over!\n");
+            gameOver = 1;
+            printf("It's a draw.\n");
+        }
+
+    }
+
+    drawBoard(&board[0]);
+}
+
+int main() {
+    srand(time(NULL));
+    run();
+}
+ 
